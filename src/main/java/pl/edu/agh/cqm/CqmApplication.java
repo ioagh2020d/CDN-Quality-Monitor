3e7c906978134e910588be00579515a8367c6847
Hubert Balcerzak <hubertbalc@gmail.com>
2021-03-18 19:37:06 +0100

    init

---
+++ src/main/java/pl/edu/agh/cqm/CqmApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.agh.cqm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CqmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CqmApplication.class, args);
+	}
+
+}
