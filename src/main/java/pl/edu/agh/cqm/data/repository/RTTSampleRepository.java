5d8afa9d86f4262a067d808eb2c24f72efa239ca
Hubert Balcerzak <hubertbalc@gmail.com>
2021-03-25 17:35:56 +0100

    Add data model

---
+++ src/main/java/pl/edu/agh/cqm/data/repository/RTTSampleRepository.java
@@ -0,0 +1,9 @@
+package pl.edu.agh.cqm.data.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pl.edu.agh.cqm.data.model.RTTSample;
+
+@Repository
+public interface RTTSampleRepository extends JpaRepository<RTTSample, Long> {
+}
