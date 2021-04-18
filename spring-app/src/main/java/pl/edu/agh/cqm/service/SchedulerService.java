03434adfcdb08f8f75d2fa306c9035f27ef691e6
MatiXOfficial <mateuszkocot99@gmail.com>
2021-04-18 21:23:56 +0200

    Scheduler delay is now updated with update-parameters

--- spring-app/src/main/java/pl/edu/agh/cqm/service/SchedulerService.java
+++ spring-app/src/main/java/pl/edu/agh/cqm/service/SchedulerService.java
@@ -1,27 +1,32 @@
 package pl.edu.agh.cqm.service;
 
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import pl.edu.agh.cqm.configuration.CqmConfiguration;
 
 @Service
 @Configuration
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class SchedulerService {
     private final PingService pingService;
     private final ThroughputService throughputService;
+    private final CqmConfiguration cqmConfiguration;
 
+    private int schedulePingCounter = 0;
 
     @Scheduled(fixedDelay = 1)
     public void scheduledThroughput(){
         throughputService.doMeasurement();
     }
 
-
-    @Scheduled(fixedRateString = "#{cqmConfiguration.getActiveSamplingRate() * 60 * 1000}")
-    public void scheduledPing(){
-        pingService.doMeasurement();
+    @Scheduled(fixedDelay = 60000)
+    public void schedulePing() {
+        if (schedulePingCounter == 0) {
+            pingService.doMeasurement();
+            schedulePingCounter = cqmConfiguration.getActiveSamplingRate();
+        }
+        schedulePingCounter--;
     }
-
 }
