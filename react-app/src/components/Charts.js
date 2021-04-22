c2308efb9caf280a6bb9124c1fc2d535274af390
lewelyn7 <karol.hamielec@gmail.com>
2021-04-22 17:25:35 +0200

    chart headers

--- react-app/src/components/Charts.js
+++ react-app/src/components/Charts.js
@@ -1,11 +1,14 @@
 import {Card} from "@material-ui/core";
 import SingleChartRTT from "./SingleChartRTT";
 import SingleChartTput from "./SingleChartTput";
+import { Typography } from '@material-ui/core';
 const Charts = () => {
 
   return (
-    <Card>
+    <Card style={{ textAlign: "center"}}>
+      <Typography variant="h6">RTT</Typography>
       <SingleChartRTT />
+      <Typography variant="h6">Throughput</Typography>
       <SingleChartTput />
     </Card>
   )
